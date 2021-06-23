7a1003cbcf6c1d3b94d6c857cffd036eadae6630
solenemhep <solene.pettier@gmail.com>
2021-06-23 14:18:37 +0200

    ok

--- src/components/componentsToBuy/AddToBuy.js
+++ src/components/componentsToBuy/AddToBuy.js
@@ -16,43 +16,78 @@ const AddToBuy = (props) => {
     { text: "vin", emoji: "🍷" },
     { text: "kiwi", emoji: "🥝" },
     { text: "pq", emoji: "🧻" },
-    { text: "tomates", emoji: "🍅" }
+    { text: "tomates", emoji: "🍅" },
   ]
 
   const handleFormSubmit = (event) => {
     event.preventDefault()
     const newToBuyText = event.target.elements.toBuy.value
-    if (toBuyList.some(el => el.text.trim().toLowerCase() === newToBuyText.trim().toLowerCase())) {
+    if (
+      toBuyList.some(
+        (el) =>
+          el.text.trim().toLowerCase() === newToBuyText.trim().toLowerCase()
+      )
+    ) {
       alert(`${newToBuyText} is already on the list`)
     } else {
       addToBuy(newToBuyText)
     }
     event.target.reset()
-    setFilter('')
+    setFilter("")
   }
 
   return (
     <React.Fragment>
       <form className="input-group input-group-lg" onSubmit={handleFormSubmit}>
-
-        <label className={darkMode ? 'input-group-text border-myblack bg-myblack text-white' : 'input-group-text border bg-light text-dark'} htmlFor="toBuy">
-          <span role="img" aria-hidden>➕</span>
+        <label
+          className={
+            darkMode
+              ? "input-group-text border-myblack bg-myblack text-white"
+              : "input-group-text border bg-light text-dark"
+          }
+          htmlFor="toBuy"
+        >
+          <span role="img" aria-hidden>
+            ➕
+          </span>
         </label>
-        <input className={darkMode ? 'form-control border-myblack bg-dark text-white' : 'form-control border bg-white text-dark'} id="toBuy" aria-label="toBuy" name="toBuy" required />
+        <input
+          className={
+            darkMode
+              ? "form-control border-myblack bg-dark text-white"
+              : "form-control border bg-white text-dark"
+          }
+          id="toBuy"
+          aria-label="toBuy"
+          name="toBuy"
+          required
+        />
 
-        <button type="submit" className={darkMode ? 'btn border-myblack btn-myblack text-white' : 'btn border btn-light text-dark'}>
+        <button
+          type="submit"
+          className={
+            darkMode
+              ? "btn border-myblack btn-myblack text-white"
+              : "btn border btn-light text-dark"
+          }
+        >
           ajouter
-      </button>
-
+        </button>
       </form>
 
       <div className="m-3 d-flex flex-wrap">
         {populars.map((popular) => (
           <button
             key={popular.text}
-            className={darkMode ? 'btn border-myblack btn-myblack text-white m-1' : 'btn border btn-light text-dark m-1'}
+            className={
+              darkMode
+                ? "btn border-myblack btn-myblack text-white m-1"
+                : "btn border btn-light text-dark m-1"
+            }
             onClick={() => addToBuy(popular.text)}
-            disabled={toBuyList.some(el => el.text === popular.text)}
+            disabled={toBuyList.some(
+              (el) => el.text.trim().toLowerCase() === popular.text
+            )}
           >
             {popular.text}{" "}
             <span role="img" aria-hidden>
